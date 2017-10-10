56c797beea80145dcfbd30eedcd0a8e3b1d0b26e
Olivia Mo <oliviatrademark@gmail.com>
2017-10-10 17:18:40 +1100

    added votes and users

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,5 @@ class Photo < ApplicationRecord
 
   include ImageUploader::Attachment.new(:image) # adds an `image` virtual attribute
   belongs_to :user
+  acts_as_votable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  resources :photos
+
   # get 'landing/index'
   devise_for :users
 
-  root to: 'home#index'
+  root to: 'photos#index'
+
+  resources :photos do
+    member do
+      put "like", to: "photos#upvote"
+      put "dislike", to: "photos#downvote"
+    end
+  end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
